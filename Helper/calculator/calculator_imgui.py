5d8bcac9c8368e5ffc12c571df84725e5caab504
pte <felicynot@gmail.com>
2025-03-19 18:04:05 -0300

    Update calculator_imgui.py

--- Helper/calculator/calculator_imgui.py
+++ Helper/calculator/calculator_imgui.py
@@ -1,19 +1,30 @@
 import tkinter as tk
 
+result_calculated = False
+
 def click_button(value):
+    global result_calculated
+    if result_calculated:
+        entry.delete(0, tk.END)
+        result_calculated = False
     entry.insert(tk.END, value)
 
 def calculate():
+    global result_calculated
     try:
         result = eval(entry.get())
         entry.delete(0, tk.END)
         entry.insert(tk.END, result)
+        result_calculated = True
     except Exception as e:
         entry.delete(0, tk.END)
         entry.insert(tk.END, "Error")
+        result_calculated = False
 
 def clear():
+    global result_calculated
     entry.delete(0, tk.END)
+    result_calculated = False
 
 window = tk.Tk()
 window.title("Calculator : ptelism")
@@ -25,7 +36,7 @@ buttons = [
     '7', '8', '9', '/',
     '4', '5', '6', '*',
     '1', '2', '3', '-',
-    'CL', '0', '=', '+'
+    'C', '0', '=', '+'
 ]
 
 row = 1
